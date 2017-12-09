9e7b305a517935fbe035fa554c0a2c7fd79f7fc5
ekirsch <33180382+ekirsch@users.noreply.github.com>
2017-12-09 12:06:17 -0500

    9s7frd

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var url= "https://dog.ceo/api/"
-function callDogAPI(url, callback){
+function callDogAPI(url, callback, ele){
     var xhttp = new XMLHttpRequest();
 
 
@@ -7,15 +7,15 @@ function callDogAPI(url, callback){
         if (this.readyState == 4 && this.status == 200) {
           // Typical action to be performed when the document is ready:
           console.log(xhttp.response)
-            callback(this.responseText); 
+            callback(this.responseText, ele); 
         }
     };
     xhttp.open("GET", url, true)
     xhttp.send();
 }
-function createDropDown (rsp){
+function createDropDown (rsp, ele){
     var doglist = JSON.parse(rsp)
-    var dogdropdown = document.getElementById("dogbreed").parentNode.querySelector(".dropdown-menu");
+    var dogdropdown = document.getElementById(ele).parentNode.querySelector(".dropdown-menu");
     
     for (var i = doglist.message.length-1; i >= 0; i--){
         console.log(doglist.message[i])
@@ -23,22 +23,28 @@ function createDropDown (rsp){
         itemAnchor.className = "dropdown-item";
         itemAnchor.href = "#"
         itemAnchor.innerText = doglist.message[i]
+        itemAnchor.addEventListener("click", clickbreed)
         dogdropdown.appendChild(itemAnchor)
+        
     }
 }
+function clickbreed(e){
+    console.log(e)
+    document.getElementById("dogImage").innerHTML = "";
+    callDogAPI(url + "breed/" + e.target.innerHTML + "/images", createImage, "dogImage");
+}
 
-
-function createImage(rsp){
+function createImage(rsp, img){
     console.log(rsp);
-    var dogImages = JSON.parse(rsp)
-    var dogImagecont = document.getElementById("dogImage");
+    var dogImages = JSON.parse(rsp);
+    var dogImagecont = document.getElementById(img);
     for (var i = dogImages.message. length - 1; i >= 0; i--) {
-        var dogImage = document.createElement("img")
-        dogImage.src = dogImages.message[i]
-        dogImagecont.appendChild(dogImage)
+        var dogImage = document.createElement("img");
+        dogImage.src = dogImages.message[i];
+        dogImagecont.appendChild(dogImage);
     }
 }
 
 
-callDogAPI(url + "breeds/list", createDropDown);
-callDogAPI(url + "breed/hound/images", createImage);
+callDogAPI(url + "breeds/list", createDropDown, "dogbreed");
+
